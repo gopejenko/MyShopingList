eb6bf26aeba6ec12b4dd155c303f178437049db6
via <viktors.gopejenko@isma.lv>
2023-12-12 15:04:04 +0200

    Bags Recycler View

--- app/src/main/java/com/example/myshopinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/myshopinglist/data/ShopListRepositoryImpl.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.myshopinglist.domain.ShopItem
 import com.example.myshopinglist.domain.ShopListRepository
+import kotlin.random.Random
 
 object ShopListRepositoryImpl : ShopListRepository {
 
@@ -15,8 +16,8 @@ object ShopListRepositoryImpl : ShopListRepository {
     private var autoIncrementId = 0
 
     init {
-        for (i in 0 until  10){
-            val item = ShopItem("Name $i", i, true)
+        for (i in 0 until  1000){
+            val item = ShopItem("Name $i", i, Random.nextBoolean())
             addShopItem(item)
         }
     }
